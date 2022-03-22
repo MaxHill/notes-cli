1ba1649cc324ba8b10a2ff6269ef616e7d3c6c54
Max Hill <max@hilloco.se>
2022-03-22 15:08:27 +0100

    fix(commitlint): completely disable body-length commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => /^chore\(release\):\ \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
+  rules: {
+    'body-max-line-length': [0, 'always']
+  } 
 }
 
